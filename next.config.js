4077141a3673561e2880da9bca48e86c4e4b81bf
Sohail Khan <sohail.khan@particle41.com>
2023-11-21 20:11:41 +0530

    Updated configurations

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const withBuilderDevTools = require("@builder.io/dev-tools/next")();
 /** @type {import('next').NextConfig} */
 const nextConfig = withBuilderDevTools({
     images: {
-        domains: ['podiumaudio.com'], // Add the domains for your images
+        // domains: ['podiumaudio.com'], // Add the domains for your images, deprecated
+        remotePatterns: [{
+            'hostname': 'podiumaudio.com'
+        }]
     },
 });
 
